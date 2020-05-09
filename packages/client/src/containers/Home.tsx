3f69829f234d1ef58eaa738c8414e849ec56348d
Pablo Pesce <papesce@gmail.com>
2020-05-09 12:15:00 -0300

    before adding the chart

--- packages/client/src/containers/Home.tsx
+++ packages/client/src/containers/Home.tsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+import './Home.css';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <button>New Job Offer</button>
-                <div>offers Graph</div>
+                <div className="new-job">
+                    <Button  variant="contained" color="primary">
+                    New Job
+                    </Button>
+                </div>
+                <div className="offers-graph">
+                    Offers Graph
+                </div>
             
             </div>
         )
