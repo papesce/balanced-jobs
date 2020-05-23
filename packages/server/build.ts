29913b53818df8f56bb32a10810e2f1744248ce4
Pablo Pesce <papesce@gmail.com>
2020-05-23 13:45:04 -0300

    fix api case

--- packages/server/build.ts
+++ packages/server/build.ts
@@ -6,4 +6,4 @@ s.rm('-rf', outDir);
 s.mkdir(outDir);
 s.cp('.env', `${outDir}/.env`);
 s.mkdir('-p', `${outDir}/api/swagger`);
-s.cp('src/api/swagger/Api.yaml', `${outDir}/api/swagger/Api.yaml`);
+s.cp('src/api/swagger/api.yaml', `${outDir}/api/swagger/api.yaml`);
